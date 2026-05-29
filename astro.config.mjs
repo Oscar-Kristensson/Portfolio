71e7500343fc4c2ef8b20930fc80ae1582f8b560
Oscar-Kristensson <kristenssonomg@gmail.com>
2026-05-29 18:18:03 +0200

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://oscar-kristensson.github.io',
+  base: 'Portfolio', // omit if using a custom domain or user site
+});
