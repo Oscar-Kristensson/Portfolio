6fc67e895240788f829a8c91dd5a382f9b36fbd4
Oscar-Kristensson <kristenssonomg@gmail.com>
2026-05-29 15:47:50 +0200

    Added the Cards as well as animations

--- src/logic/structs.ts
+++ src/logic/structs.ts
@@ -1,7 +1,16 @@
 export class CardData {
     constructor(
         public title: string,
-        public items: string[],
+        public items: CardDataItem[],
+
+    ) {}
+}
+
+
+
+export class CardDataItem {
+    constructor(
+        public description: string,
 
     ) {}
 }
