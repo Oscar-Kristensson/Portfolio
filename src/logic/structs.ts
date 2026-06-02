37c00a09f908a9d853425919a1af8bcd2b6f1d53
Oscar-Kristensson <kristenssonomg@gmail.com>
2026-06-02 21:10:54 +0200

    Added the project section

--- src/logic/structs.ts
+++ src/logic/structs.ts
@@ -2,7 +2,7 @@ export class CardData {
     constructor(
         public title: string,
         public items: CardDataItem[],
-        public link: string,
+        public link?: string,
 
     ) {}
 }
