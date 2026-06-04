4e21506ef3de355bf94946241173d02e7b07cb7d
Oscar-Kristensson <kristenssonomg@gmail.com>
2026-06-04 10:03:23 +0200

    Added the TouchScroller for better vertical scrolling

---
+++ src/logic/scroll.ts
@@ -0,0 +1,50 @@
+
+
+export class TouchScroller {
+    scrollLeft: number;
+
+
+    constructor(
+        private timeline: HTMLElement,
+        private isDown: boolean = false,         
+        private startX: number = 0,         
+    ) {
+        this.scrollLeft = this.timeline.scrollLeft;
+
+        this.timeline.classList.add("touchScroller");
+
+        this.timeline.addEventListener("mousedown", (e) => this.onMouseDown(e));
+        this.timeline.addEventListener("mouseleave", () => this.onMouseLeave());
+        this.timeline.addEventListener("mouseup", () => this.onMouseUp());
+        this.timeline.addEventListener("mousemove", (e) => this.onMouseMove(e));
+    }
+
+    onMouseDown(e: any) {
+        this.isDown = true;
+        this.timeline.classList.add("dragging");
+
+        this.startX = e.pageX - this.timeline.offsetLeft;
+        this.scrollLeft = this.timeline.scrollLeft;
+    }
+
+    onMouseLeave() {
+        this.isDown = false;
+        this.timeline.classList.remove("dragging");
+    }
+
+    onMouseUp() {
+        this.isDown = false;
+        this.timeline.classList.remove("dragging");    
+    }
+
+    onMouseMove(e: any) {
+        if (!this.isDown) return;
+
+        e.preventDefault();
+
+        const x = e.pageX - this.timeline.offsetLeft;
+        const walk = (x - this.startX) * 1; // drag speed multiplier
+
+        this.timeline.scrollLeft = this.scrollLeft - walk;
+    } 
+}
